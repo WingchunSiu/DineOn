fcd72e72a51b325f6c0d1807d932f8e6b9159850
notrichardpeng <notrichardpeng@gmail.com>
2025-02-16 00:07:34 -0800

    Prevent top bar from hiding time

--- my-app/app/Menu/_layout.tsx
+++ my-app/app/Menu/_layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Colors } from '@/constants/Colors';
+import { Stack } from 'expo-router';
 
 import BreakfastTab from './breakfast';
 import LunchTab from './lunch';
@@ -13,18 +14,21 @@ export default function MenuTabsLayout() {
   const colorScheme = useColorScheme();  
 
   return (
-    <Tab.Navigator
-      screenOptions={{ 
-        swipeEnabled: true,       
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        tabBarIndicatorStyle: { backgroundColor: "#990000" },
-        tabBarLabelStyle: { color: 'black' },
-        tabBarStyle: { backgroundColor: Colors[colorScheme ?? 'light'].background },
-      }}
-    >
-      <Tab.Screen name="Breakfast" component={BreakfastTab} />
-      <Tab.Screen name="Lunch" component={LunchTab} />
-      <Tab.Screen name="Dinner" component={DinnerTab} />
-    </Tab.Navigator>
+    <>
+      <Stack.Screen name="Menu" options={{ headerShown: false }} />        
+      <Tab.Navigator
+        screenOptions={{   
+          swipeEnabled: true,       
+          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+          tabBarIndicatorStyle: { backgroundColor: "#990000" },
+          tabBarLabelStyle: { color: 'black' },
+          tabBarStyle: { backgroundColor: Colors[colorScheme ?? 'light'].background },
+        }}
+      >      
+        <Tab.Screen name="Breakfast" component={BreakfastTab} />
+        <Tab.Screen name="Lunch" component={LunchTab} />
+        <Tab.Screen name="Dinner" component={DinnerTab} />
+      </Tab.Navigator>
+    </>
   );
 }
